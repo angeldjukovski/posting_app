13407b2c089eeacc65c89e68d6fed25658d6eeeb
Angel Djukovski <angeldjukovski@hotmail.com>
2026-01-04 14:17:06 +0100

    Database setup

--- poster-back/src/app.module.ts
+++ poster-back/src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+    }),
+    AuthModule,
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
