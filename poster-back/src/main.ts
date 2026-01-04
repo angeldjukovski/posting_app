66daf2c15381ede3d170a7c4a3825f160727c51b
Angel Djukovski <angeldjukovski@hotmail.com>
2026-01-04 17:08:30 +0100

    Database setup update

--- poster-back/src/main.ts
+++ poster-back/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:4200',
+      methods: 'GET,POST,PUT,PATCH,HEAD,DELETE',
+      allowedHeaders: 'Content-Type, Authorization',
+      credentials: true,
+    },
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Poster Site API')
+    .setDescription('Used for making posts')
+    .setVersion('1.0')
+    .addBearerAuth()    
+    .build();     
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
